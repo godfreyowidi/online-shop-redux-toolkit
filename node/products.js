86fe2beaa690a20afe89b0a3b871fafc7729ff31
GOdfrey Owidi <godfreyowiidi@gmail.com>
2022-01-04 16:54:11 +0300

    add online shop

---
+++ node/products.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: 1,
+    name: "Spirulina Tablets",
+    description: "a range of vitamins and minerals essential for maintaining a healthy immune system, like vitamins E, C, and B6",
+    price: 1999,
+    image: "https://res.cloudinary.com/drczzyzar/image/upload/v1640273328/online-shop/spirulina_wpenzz.jpg"
+  },
+  {
+    id: 1,
+    name: "Moringa Tea",
+    description: "rich in antioxidants, which are primarily the polyphenols or plant compounds in it",
+    price: 1999,
+    image: "https://res.cloudinary.com/drczzyzar/image/upload/v1640273311/online-shop/moringa_a3ulau.jpg"
+  },
+  {
+    id: 1,
+    name: "Marijuana Chocolate",
+    description: "help regulate and prevent diabetes as well as aiding body in regulating insulin while managing caloric intake efficiently",
+    price: 1999,
+    image: "https://res.cloudinary.com/drczzyzar/image/upload/v1640273319/online-shop/marijuana_ny0zjj.jpg"
+  },
+];
+
+module.exports = products;
